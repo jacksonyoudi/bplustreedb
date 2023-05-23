c65218310cfcebe082ac69ee2def1008436a61e5
youdi <liangchangyoujackson@gmail.com>
2023-05-23 22:58:12 +0800

    db

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[allow(clippy::mutable_key_type)]
+mod bucket;
+mod bytes;
+mod cursor;
+mod data;
+mod db;
+mod errors;
+mod freelist;
+mod lifetimes;
+mod meta;
+mod node;
+mod page;
+mod page_node;
+mod tx;
